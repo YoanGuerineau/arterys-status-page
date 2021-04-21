d09e19c0279880954a7a93b639734e8141252c53
YoanGuerineau <yoan.guerineau@live.fr>
2021-04-21 12:31:04 +0200

    Fixing url

--- .github/workflows/fetch_comments.sh
+++ .github/workflows/fetch_comments.sh
@@ -1,3 +1,3 @@
 while IFS= read line; do
-    wget https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$line -O ./asset/json/comments/$line-comments.json
+    wget https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$line/comments -O ./asset/json/comments/$line-comments.json
 done <<< $(cat ./asset/json/issues.json | jq -r '.[].number')
