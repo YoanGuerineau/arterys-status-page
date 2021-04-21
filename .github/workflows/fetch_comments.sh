d7a4e6e8364b19316f4bd2e7d514961c0f95cc1a
YoanGuerineau <yoan.guerineau@live.fr>
2021-04-21 12:28:11 +0200

    Fix

--- .github/workflows/fetch_comments.sh
+++ .github/workflows/fetch_comments.sh
@@ -1,3 +1,3 @@
 while IFS= read line; do
     wget https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$line -O ./asset/json/comments/$line-comments.json
-done <<< $(cat ../../asset/json/issues.json | jq -r '.[].number')
+done <<< $(cat ./asset/json/issues.json | jq -r '.[].number')
