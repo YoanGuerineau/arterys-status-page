ce45d27adec10a82e7bcb8da487c930c214e92db
YoanGuerineau <yoan.guerineau@live.fr>
2021-04-21 13:47:24 +0200

    Updating individual issue content

--- asset/js/issue-script.js
+++ asset/js/issue-script.js
@@ -1,11 +1,18 @@
 const urlParams = new URLSearchParams(window.location.search);
 const issue_id = urlParams.get('issue_id');
 const issue_header = document.getElementById('individual-issue-header');
-$.getJSON('https://api.github.com/repos/yoanguerineau/arterys-status-page/issues/'+issue_id, function (issue) {
+$.getJSON('./asset/json/issues.json', function (data) {
+    let issue;
+    for(let i = 0; i < data.length; i++){
+        if(data[i].number == issue_id){
+            issue=data[i];
+            break;
+        }
+    }
     fillIssues([issue], issue_header, individual = true);
 });
 
 const issue_comments = document.getElementById('individual-issue-comments');
-$.getJSON('https://api.github.com/repos/yoanguerineau/arterys-status-page/issues/'+issue_id+'/comments', function (comments) {
+$.getJSON('./asset/json/comments/'+issue_id+'-comments.json', function (comments) {
     fillComments(comments, issue_comments);
 });
