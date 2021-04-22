5ad1ee5e47257b2e25db1b4d9e3803b832535c99
Yoan Guerineau <39827510+YoanGuerineau@users.noreply.github.com>
2021-04-23 00:27:59 +0200

    Update fetch_comments.sh

--- .github/workflows/fetch_comments.sh
+++ .github/workflows/fetch_comments.sh
@@ -1,7 +1,5 @@
 while IFS= read line; do
     comments=$(jq --argjson line $line -r '.[] | select(.number==$line) | .comments' ./asset/json/issues.json)
-    echo $line
-    echo $comments
     if [[ "$comments" != "0" ]]; then
         wget https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$line/comments -O ./asset/json/comments/$line-comments.json
     fi
