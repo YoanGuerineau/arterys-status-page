9ea5346ec2793e79152d901c21ca85eeacdd5646
Yoan Guerineau <39827510+YoanGuerineau@users.noreply.github.com>
2021-05-28 17:22:37 +0200

    Update filter_json.js

--- .github/workflows/filter_json.js
+++ .github/workflows/filter_json.js
@@ -4,6 +4,14 @@ const fs = require('fs')
 let raw_data = fs.readFileSync('./asset/json/displayed-issues.json')
 let issues = JSON.parse(raw_data)
 issues.forEach( (issue,index) => {
-  console.log(issue)
-  console.log(index)
+  console.log(JSON.stringify(clean_object(issue)))
+  //console.log(index)
 })
+
+function clean_object(object): Object {
+  let labels = object.labels
+  labels.forEach( (label,index) => { 
+    labels[index] = Object(label.name)
+  })
+  return Object(object.html_url,object.comments_url,object.number,object.title,object.user.login,labels,object.state,object.comments,object.created_at,object.body)
+}
