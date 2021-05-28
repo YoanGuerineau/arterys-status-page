b25b8f1fb385173dc4bb7e103cae53562ba1e334
Yoan Guerineau <39827510+YoanGuerineau@users.noreply.github.com>
2021-05-28 17:04:59 +0200

    Update filter_json.js

--- .github/workflows/filter_json.js
+++ .github/workflows/filter_json.js
@@ -2,5 +2,5 @@
 
 const fs = require('fs')
 let raw_data = fs.readFileSync('./asset/json/displayed-issues.json')
-let issues = JSON.parse(rawdata)
+let issues = JSON.parse(raw_data)
 console.log(issues)
