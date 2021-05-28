44f0d0761b67f4ebe5cc66842b3f9f720f34c13d
Yoan Guerineau <39827510+YoanGuerineau@users.noreply.github.com>
2021-05-28 22:59:59 +0200

    Update cleanup_comments_json.js

--- .github/workflows/cleanup_comments_json.js
+++ .github/workflows/cleanup_comments_json.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fs = require('fs');
-const args = process.argv.slice(2)[0].split(/[\n, ]+/).filter(element => return element.length !== 0;
+const args = process.argv.slice(2)[0].split(/[\n, ]+/).filter(element => { return element.length !== 0; });
 
 
 args.forEach( file => {
