7a7f1150b5967112ddc2f032a43873215579b647
Yoan Guerineau <39827510+YoanGuerineau@users.noreply.github.com>
2021-05-28 21:02:33 +0200

    Create cleanup_comment_json.js

---
+++ .github/workflows/cleanup_comment_json.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+args.forEach( file => {
+  const raw_data = fs.readFileSync("./asset/json/comments/"+file);
+  const comment = JSON.parse(raw_data);
+
+  cleanup_comment(comment);
+
+  const result_json = JSON.stringify(issues, null, 2);
+  fs.writeFileSync("./asset/json/comments/"+file, result_json);  
+});
+
+
+function cleanup_comment(comment) {
+  return Object({
+    id: comment.id,
+    body: comment.body,
+    user: Object({login: comment.user.login}),
+    created_at: comment.created_at,
+  });
+}
